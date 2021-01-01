416bf5849a1b38c328c50546bf3951e03c3aa696
Mr_p0tat0 <59037001+Mr-p0tat0@users.noreply.github.com>
2021-01-01 14:55:29 +0530

    Create New year.py

---
+++ New year.py
@@ -0,0 +1,21 @@
+/**** HAPPY NEW YEAR *****/
+Import happiness;
+Import health;
+Import wealth;
+Import new hack;
+Import new bug;
+
+Public class p0tat0
+{
+  Time = “Begin 2021”
+  while (time <= “End 2020”)
+  {
+    push(love);
+    push(joy);
+    push(happiness);
+    time++;
+  }
+  if (bad_memorties == 1)
+    pop(all);
+  System.out.println(“wish you a very happy and prosperous NEW YEAR”);
+}
